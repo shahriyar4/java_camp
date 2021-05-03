55b3de1816d1809c95a3436979320aa3db98e0a7
Shahriyar Hasanov <shahrihasan44@gmail.com>
2021-05-03 16:47:19 +0400

    Java Kamp Kodlari

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/2c/300adef705ac001b14dec67cf9bf0f4f
@@ -0,0 +1,10 @@
+package project;
+
+public class first {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ project/src/project/Product.java
@@ -0,0 +1,23 @@
+package project;
+
+public class Product {
+
+	public Product() {
+		System.out.println("Ben worked");
+	}
+
+	public Product(int id, String name, double unitPrice, String detail) {
+		
+		this();
+		this.id = id;
+		this.name = name;
+		this.unitPrice = unitPrice;
+		this.detail = detail;
+	}
+
+	int id;
+	String name;
+	double unitPrice;
+	String detail;
+
+}
